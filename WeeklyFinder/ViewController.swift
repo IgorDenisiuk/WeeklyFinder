3bbfd246edf0fecb382bc68464f978dad0ec7da4
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2020-12-06 22:12:00 +0300

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Igor Denisiuk on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
